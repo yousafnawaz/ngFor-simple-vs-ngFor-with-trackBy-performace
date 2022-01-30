f657dc7cfde4d8f777cea5778f747265735e26f4
yousafnawaz <yousafnawaz@hotmail.com>
2022-01-30 19:50:39 +0500

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component, VERSION } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2s ease-in', style({ opacity: 1, color: '#198754' })),
+      ]),
+      transition(':leave', [
+        animate(
+          '2s 3s ease-out',
+          style({
+            opacity: 0,
+            color: '#dc3545',
+            'text-decoration': 'line-through',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit, OnDestroy {
+  timerId: number;
+  items: Item[] = [
+    {
+      id: 1,
+      name: 'Item 1',
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+    },
+    {
+      id: 4,
+      name: 'Item 4',
+    },
+    {
+      id: 5,
+      name: 'Item 5',
+    },
+  ];
+
+  ngOnInit(): void {
+    this.initialize();
+  }
+  initialize() {
+    //fake API call to simulate scenario
+    this.timerId = setTimeout(() => {
+      // new data arriives from server
+      this.items = [
+        {
+          id: 1,
+          name: 'Item 1',
+        },
+        {
+          id: 2,
+          name: 'Item 2',
+        },
+        {
+          id: 3,
+          name: 'Item 3 Updated',
+        },
+        {
+          id: 6,
+          name: 'Item 6 Added',
+        },
+      ];
+    }, 5000);
+  }
+
+  trackByItemId(index: number, item: Item): number {
+    return item.id;
+  }
+  replay() {
+    this.clearTimerRefernce();
+    this.initialize();
+  }
+  ngOnDestroy(): void {
+    this.clearTimerRefernce();
+  }
+  clearTimerRefernce() {
+    clearTimeout(this.timerId);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
